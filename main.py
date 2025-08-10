a1e06c5e0e3a624c785073638d8d915ffe11823e
coekmez <coekmez.mert@proton.me>
2025-08-10 13:39:53 +0200

    day 4 part 2 wip

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import importlib
 import inspect
+import os
 
 YEARS = ['2023']
+SOLVE_EXAMPLE = False
 
 if __name__ == "__main__":
-    print("Results for 2023")
-    for i in range(1,5):
-        # import the day module
-        curr_day = importlib.import_module(f"year_2023.day_{i}")
-        # load data
-        with open(f'year_2023/dataset/day_{i}.txt', 'r') as f:
-            data = f.read().splitlines()
-        # display solution
-        print(25*'-')
-        print(f"Results for Day {i}")
-        curr_day.solve(data)
+    for year in YEARS:
+        print(5*'#',f"YEAR {year}", 5*'#')
+        for i in range(1,5):
+            # import the day module
+            curr_day = importlib.import_module(f"year_{year}.day_{i}")
+            # load data
+            if SOLVE_EXAMPLE:
+                root_path = f'year_{year}/examples'
+            else:
+                root_path =f'year_{year}/dataset'
+            with open(os.path.join(root_path, f'day_{i}.txt'), 'r') as f:
+                data = f.read().splitlines()
+            # display solution
+            print(21*'-')
+            print(f"Results for Day {i}")
+            curr_day.solve(data)
 
 
 
--- year_2023/day_4.py
+++ year_2023/day_4.py
@@ -1,23 +1,34 @@
 def part_1(data):
     total = 0
     for line in data:
-        found_start_pos = False
         my_numbers = []
         curr_num = []
-        for c in line:
-            if c != ':' and not found_start_pos:
+        winning_numbers = set()
+        counting_winning_numbers = False
+        counting_my_numbers = False
+        for i in range(len(line)):
+            c = line[i]
+            if c != ':' and not counting_winning_numbers and not counting_my_numbers:
                 continue
             elif c == ':':
-                found_start_pos = True
+                counting_winning_numbers = True
+                continue
+            elif c == '|':
+                counting_winning_numbers = False
+                counting_my_numbers = True
                 continue
             else:
                 if c.isdigit():
-                    curr_num.append()
-                if c == ' ' and len(curr_num) > 0:
-                    pass
-
-
-
+                    curr_num.append(c)
+                if (c == ' ' or i==len(line)-1) and len(curr_num) > 0:
+                    if counting_winning_numbers:
+                        winning_numbers.add(int(''.join(curr_num)))
+                    elif counting_my_numbers:
+                        my_numbers.append(int(''.join(curr_num)))
+                    else:
+                        raise ValueError('You should not be here. You are either checking winning numbers or my numbers.')
+                    curr_num = []
+        # get the leftover curr_num if there is any
         occurrences = 0
         for num in my_numbers:
             if num in winning_numbers:
@@ -30,6 +41,49 @@ def part_1(data):
 
     return total
 
+def part_2(data):
+    total = 0
+    nb_of_cards = len(data) * [1]
+    for i in range(len(data)):
+        line = data[i]
+        my_numbers = []
+        curr_num = []
+        winning_numbers = set()
+        counting_winning_numbers = False
+        counting_my_numbers = False
+        for char_idx in range(len(line)):
+            c = line[char_idx]
+            if c != ':' and not counting_winning_numbers and not counting_my_numbers:
+                continue
+            elif c == ':':
+                counting_winning_numbers = True
+                continue
+            elif c == '|':
+                counting_winning_numbers = False
+                counting_my_numbers = True
+                continue
+            else:
+                if c.isdigit():
+                    curr_num.append(c)
+                if (c == ' ' or char_idx==len(line)-1) and len(curr_num) > 0:
+                    if counting_winning_numbers:
+                        winning_numbers.add(int(''.join(curr_num)))
+                    elif counting_my_numbers:
+                        my_numbers.append(int(''.join(curr_num)))
+                    else:
+                        raise ValueError('You should not be here. You are either checking winning numbers or my numbers.')
+                    curr_num = []
+        # get the leftover curr_num if there is any
+        occurrences = 0
+        for num in my_numbers:
+            if num in winning_numbers:
+                occurrences += 1
+        for j in range(occurrences):
+            if j+i+1 < len(nb_of_cards):
+                nb_of_cards[j+i+1] += nb_of_cards[i]
+        pass
+    return sum(nb_of_cards)
+
 def solve(data):
     print(f"Part_1: {part_1(data)}")
-    # print(f"Part_2: {part_2(data)}")
+    print(f"Part_2: {part_2(data)}")
